97c55ea727dcb26f77d926a09f70f277ea4eb3e4
Dashulietta <108624114+Dashulietta@users.noreply.github.com>
2022-10-23 14:01:40 +0300

    changed wetather function

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 export default function Weather() {
-    function handleResponse(response){
-        alert (`The weather in London is ${response.data.main.temp}C`);
+    const [city, setCity] = useState(null);
+    const [loaded, setLoaded] = useState(false);
+    const [weather, setWeather] = useState(null);
+  
+    function displayWeather(response) {
+      setLoaded(true);
+      setWeather({
+        temperature: response.data.main.temp,
+        wind: response.data.wind.speed,
+        humidity: response.data.main.humidity,
+        description: response.data.weather[0].description,
+        icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      });
     }
-    let apiKey = "9a26aeeafb7f3d9e16e58f5effdd59d0";
-    let units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(handleResponse);
-    return "Search"
-}
+    function handleSubmit(event) {
+      event.preventDefault();
+      let apiKey = "9a26aeeafb7f3d9e16e58f5effdd59d0";
+      let units = "metric";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+      axios.get(apiUrl).then(displayWeather);
+    }
+    function updateCity(event) {
+      setCity(event.target.value);
+    }
+    let form = (
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Type a city" onChange={updateCity} />
+        <button type="submit"> Search </button>
+      </form>
+    );
+    if (loaded) {
+      return (
+        <div>
+          {form}
+          <ul>
+            <li> Temperature: {Math.round(weather.temperature)}℃ </li>
+            <li> Description: {weather.description} % </li>
+            <li> Humidity: {weather.humidity} % </li>
+            <li> Wind: {weather.wind} km/h</li>
+            <li>
+              <img src={weather.icon} alt={weather.description} />{" "}
+            </li>
+          </ul>
+        </div>
+      );
+    } else {
+      return form;
+    }
+  }
+  
