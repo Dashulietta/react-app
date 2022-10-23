a7c3470a04a2f6e8171163abf850e95bbfceb96b
Dashulietta <108624114+Dashulietta@users.noreply.github.com>
2022-10-23 13:57:30 +0300

    added test function alerting temp

--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,13 @@
+import React from "react";
+import axios from "axios";
+
 export default function Weather() {
+    function handleResponse(response){
+        alert (`The weather in London is ${response.data.main.temp}C`);
+    }
+    let apiKey = "9a26aeeafb7f3d9e16e58f5effdd59d0";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
     return "Search"
 }
